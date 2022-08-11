3c7c9a2817db2ed269d0e0f3fd791fdba069c60d
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-08-11 17:43:35 +0300

    Created a database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mariadb',
+        host: 'localhost',
+        port: 3306,
+        username: configService.getOrThrow('MYSQL_USER'),
+        password: configService.getOrThrow('MYSQL_PASSWORD'),
+        database: configService.getOrThrow('MYSQL_DATABASE'),
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
