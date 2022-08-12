280a4156724e5fb681bc152fcfb8440c2c1d4c33
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-08-12 13:56:58 +0300

    Registration CategoryORM in category module

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
+import { CategoryORM } from './categoryORM';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([CategoryORM])],
   controllers: [CategoryController],
   providers: [CategoryService]
 })
