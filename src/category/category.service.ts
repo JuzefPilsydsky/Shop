a3e133776e22127a4fa2814c876e0f7295d6771f
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-08-12 14:45:46 +0300

    Created category servise

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CategoryDto } from './categoryDto';
+import { CategoryORM } from './categoryORM';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+    constructor(
+        @InjectRepository(CategoryORM)
+        private categoryReposytory: Repository<CategoryORM>
+    ) {}
+
+    getAll(): Promise<CategoryORM[]> {
+        return this.categoryReposytory.find()
+    }
+
+    getOne(id: number): Promise<CategoryORM | null> {
+        return this.categoryReposytory.findOneBy({id})
+    }
+
+    create(categoryDto: CategoryDto) {
+        return this.categoryReposytory.save(categoryDto)
+    }
+
+    async delete(id: number): Promise<void> {
+        await this.categoryReposytory.delete(id)
+    }
+
+    async updateOneById(id: number, categoryDto: CategoryDto) {
+        await this.categoryReposytory.update(id, categoryDto)
+    }
+}
