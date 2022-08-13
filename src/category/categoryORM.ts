8b4e354240633ac20556ae4bdedb844311aa42b0
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-08-13 14:07:17 +0300

    Created one-to-many and many-to-one relations

--- src/category/categoryORM.ts
+++ src/category/categoryORM.ts
@@ -1,9 +1,11 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { ProductORM } from "src/product/productORM";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Category } from "./category.interface";
 
 @Entity({name: "Category"})
 export class CategoryORM implements Category {
     @PrimaryGeneratedColumn()
+    @OneToMany((type) => ProductORM, category => category.categoryId )
     id: number;
     @Column()
     name: string;
