8e496d1a6e6357b04f20e73b17f7cbe65a0d1687
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-08-13 12:33:19 +0300

    Creating CRUD methods

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { CategoryDto } from './categoryDto';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+    constructor(private categoryServise: CategoryService) {}
+
+    @Get()
+    getAll() {
+        return this.categoryServise.getAll()
+    }
+
+    @Get(':id')
+    getOne(@Param() param) {
+        return this.categoryServise.getOne(param.id)
+    }
+
+    @Post('create')
+    create(@Body() categoryDto: CategoryDto) {
+        return this.categoryServise.create(categoryDto)
+    }
+
+    @Delete(':id')
+    delete(@Param() param) {
+        return this.categoryServise.delete(param.id)
+    }
+
+    @Put(':id')
+    async update(@Body() categoryDto: CategoryDto, @Param() param) {
+        const update = await this.categoryServise.update(param.id, categoryDto);
+        return update
+    }
+}
