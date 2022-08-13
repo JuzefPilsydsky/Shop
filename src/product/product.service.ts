00302cca8d1c999e603d4185208d464fc0eedb5d
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-08-13 14:54:04 +0300

    Created service for products

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CategoryService } from 'src/category/category.service';
+import { Repository } from 'typeorm';
+import { ProductDto } from './productDto';
+import { ProductORM } from './productORM';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {constructor(
+    @InjectRepository(ProductORM)
+    private productReposytory: Repository<ProductORM>,
+    
+    @Inject(CategoryService)
+    private categoryService: CategoryService
+) {}
+
+getAll(): Promise<ProductORM[]> {
+    return this.productReposytory.find();
+}
+
+getOne( id: number ): Promise<ProductORM> {
+    return this.productReposytory.findOneBy({id})
+}
+
+async create(productDto: ProductDto) {
+    const category = await this.categoryService.getOne(productDto.categoryId.id)
+
+    if (category == null) {
+        throw new Error()
+    }
+
+    return this.productReposytory.save({
+        ...productDto,
+        categoryId: category
+    })
+}
+
+async delete( id: number ): Promise<void> {
+    await this.productReposytory.delete(id)
+}
+
+async update(id: number, productDto: ProductDto) {
+    await this.productReposytory.update(id, productDto)
+}}
