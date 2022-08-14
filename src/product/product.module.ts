030e3ad506b7846ad9b51507f88f8568ab6b7979
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-08-14 11:46:35 +0300

    Implementation category service and ORM in product module

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -3,12 +3,14 @@ import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductORM } from './productORM';
+import { CategoryService } from 'src/category/category.service';
+import { CategoryORM } from 'src/category/categoryORM';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([ProductORM])
+    TypeOrmModule.forFeature([ProductORM, CategoryORM])
   ],
-  providers: [ProductService],
+  providers: [ProductService, CategoryService],
   controllers: [ProductController]
 })
 export class ProductModule {}
