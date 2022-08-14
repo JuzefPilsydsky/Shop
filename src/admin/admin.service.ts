50b7be32ba1a7378c38858e2b32d19f67db6dd93
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-08-14 16:43:32 +0300

    Created service for admin entity

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AdminDto } from './adminDto';
+import { AdminORM } from './adminORM';
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+    constructor(
+        @InjectRepository(AdminORM)
+        private adminRepository: Repository<AdminORM>
+    ) {}
+
+    getAll(): Promise<AdminORM[]> {
+        return this.adminRepository.find();
+    }
+
+    getOne(id: number): Promise<AdminORM> {
+        return this.adminRepository.findOneBy({id});
+    }
+
+    create(adminDto: AdminDto) {
+        return this.adminRepository.save(adminDto);
+    }
+
+    async delete(id: number): Promise<void> {
+        await this.adminRepository.delete(id);
+    }
+
+    async update(id: number, adminDto: AdminDto) {
+        await this.adminRepository.update(id, adminDto);
+    }
+
+}
