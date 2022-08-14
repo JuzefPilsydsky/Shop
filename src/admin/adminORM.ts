e0332229d6147d5ff483676652195dd6b7fe3a91
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-08-14 15:40:27 +0300

    Created ORM for admin entity

---
+++ src/admin/adminORM.ts
@@ -0,0 +1,15 @@
+import { Column, Entity, PrimaryGeneratedColumn, Unique } from "typeorm";
+import { Admin } from "./admin.interface";
+
+@Entity({name: "Admin"})
+@Unique(["email"])
+export class AdminORM implements Admin {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  email: string;
+  @Column()
+  password: string;
+  @Column()
+  name: string;
+}
