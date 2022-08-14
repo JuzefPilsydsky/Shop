aa2fe077c4ac87d0190b2cc26503b94a78a0b3dd
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-08-14 16:42:58 +0300

    Created controller for admin entity

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { AdminService } from './admin.service';
+import { AdminDto } from './adminDto';
 
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+    constructor(private adminSevrice: AdminService) {}
+
+    @Get()
+    getAll() {
+        return this.adminSevrice.getAll();
+    }
+
+    @Get(':id')
+    getOne(@Param() params) {
+        return this.adminSevrice.getOne(params.id);
+    }
+
+    @Post('create')
+    create(@Body() adminDto: AdminDto) {
+        return this.adminSevrice.create(adminDto);
+    }
+
+    @Delete(':id')
+    delete(@Param() params) {
+        return this.adminSevrice.delete(params.id);
+    }
+
+    @Put(':id')
+    async update(@Param() param, @Body() adminDto: AdminDto) {
+        const response = await this.adminSevrice.update(param.id, adminDto);
+        return response;
+    }
+}
