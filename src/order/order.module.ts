d163f839e62718ff30d28d535e05f5ea5f17033c
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-08-14 20:40:28 +0300

    Implemented order and list ORM in order module

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrderListORM } from './order.list.ORM';
+import { ListOfProductsORM } from './list.of.products.ORM';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([OrderListORM,ListOfProductsORM])],
   providers: [OrderService],
   controllers: [OrderController]
 })
