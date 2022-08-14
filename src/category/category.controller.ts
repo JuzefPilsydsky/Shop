d4d1816a8835b0b11d71cc2519e64c4687df970f
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-08-14 13:34:15 +0300

    Added http status codes

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryDto } from './categoryDto';
 
@@ -17,16 +17,19 @@ export class CategoryController {
     }
 
     @Post('create')
+    @HttpCode(201)
     create(@Body() categoryDto: CategoryDto) {
         return this.categoryServise.create(categoryDto)
     }
 
     @Delete(':id')
+    @HttpCode(200)
     delete(@Param() param) {
         return this.categoryServise.delete(param.id)
     }
 
     @Put(':id')
+    @HttpCode(201)
     async update(@Body() categoryDto: CategoryDto, @Param() param) {
         const update = await this.categoryServise.update(param.id, categoryDto);
         return update
