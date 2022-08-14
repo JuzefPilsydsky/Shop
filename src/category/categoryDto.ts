818948c367a16e74788997fa48116375cebfcbd7
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-08-14 13:33:19 +0300

    Added IsSrting validation

--- src/category/categoryDto.ts
+++ src/category/categoryDto.ts
@@ -1,7 +1,8 @@
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class CategoryDto {
     @IsNotEmpty({message:'Must be a name'})
+    @IsString({message:'Name must be a string'})
     readonly name: string;
     @IsNotEmpty({message:'Must be a image'})
     readonly imgUrl: string;
