57cf3b9ea6cf367e139bb876a46812a04dfc661d
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-08-14 15:44:24 +0300

    Implements ORM in module

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdminORM } from './adminORM';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([AdminORM])
+  ],
   providers: [AdminService],
   controllers: [AdminController]
 })
