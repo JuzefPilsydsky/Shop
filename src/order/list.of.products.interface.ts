e8469adada6c599aeea11f2da8f0f53b9051fff8
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-08-16 13:50:15 +0300

    Fixed syntacs mistakes and orderId field removed

--- src/order/list.of.products.interface.ts
+++ src/order/list.of.products.interface.ts
@@ -1,11 +1,9 @@
 import { Products } from "src/product/product.interface";
-import { OrderList } from "./order.list.interface";
 
 export interface ListOfProducts {
     id: number;
     count: number;
-    prise: number;
-    currensy: string;
+    price: number;
+    currency: string;
     productId: Products;
-    orderListId: OrderList;
 }
