56ebd27a4847058412c0f5bc3579ed04085765f1
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-08-22 14:35:38 +0300

    Fixed enum

--- src/order/order.status.enum.ts
+++ src/order/order.status.enum.ts
@@ -1,7 +1,8 @@
 export enum OrderStatus {
-    confirm = 1,
-    approved = 2,
-    processed = 3,
-    preparing = 4,
-    delivered = 5
+    created,
+    confirm, 
+    approved, 
+    processed, 
+    preparing,
+    delivered, 
 } 
