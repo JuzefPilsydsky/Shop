174a505d730959a276d82779118e221d26180a4c
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-08-22 14:33:27 +0300

    Added new imports and providers in order module

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -3,11 +3,15 @@ import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderORM } from './order.ORM';
-import { ListOfProductsORM } from './list.of.products.ORM';
+import { OrderProductsORM } from './order.products.ORM';
+import { ProductORM } from 'src/product/productORM';
+import { ProductService } from 'src/product/product.service';
+import { CategoryService } from 'src/category/category.service';
+import { CategoryORM } from 'src/category/categoryORM';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([OrderORM,ListOfProductsORM])],
-  providers: [OrderService],
+  imports:[TypeOrmModule.forFeature([OrderORM, OrderProductsORM, ProductORM, CategoryORM])],
+  providers: [OrderService, ProductService, CategoryService],
   controllers: [OrderController]
 })
 export class OrderModule {}
