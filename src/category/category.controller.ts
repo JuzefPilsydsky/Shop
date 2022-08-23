4d56231e13582376626042f6aabdac3a27851acc
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-08-23 16:20:41 +0300

    Added roles decorator in post delete put methods

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,6 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put } from '@nestjs/common';
+import { Role } from 'src/admin/role.enum';
+import { Roles } from 'src/admin/roles.decorator';
 import { CategoryService } from './category.service';
 import { CategoryDto } from './categoryDto';
 
@@ -17,18 +19,21 @@ export class CategoryController {
     }
 
     @Post('create')
+    @Roles(Role.ADMIN)
     @HttpCode(201)
     create(@Body() categoryDto: CategoryDto) {
         return this.categoryService.create(categoryDto);
     }
 
     @Delete(':id')
+    @Roles(Role.ADMIN)
     @HttpCode(200)
     delete(@Param() param) {
         return this.categoryService.delete(param.id);
     }
 
     @Put(':id')
+    @Roles(Role.ADMIN)
     @HttpCode(201)
     async update(@Body() categoryDto: CategoryDto, @Param() param) {
         const update = await this.categoryService.update(param.id, categoryDto);
