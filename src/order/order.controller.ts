87fe684402af602b02c3aff203b9cfe4078aa5b5
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-08-23 14:33:49 +0300

    Created order controller

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
+import { OrderService } from './order.service';
+import { OrderDto } from './orderDto';
 
 @Controller('order')
-export class OrderController {}
+export class OrderController {
+    constructor (private orderService: OrderService) {}
+
+    @Post('create')
+    @HttpCode(201)
+    create(@Body() orderDto: OrderDto) {
+        return this.orderService.create(orderDto)
+    }
+
+    @Get(':id')
+    getOne(@Param() param) {
+        return this.orderService.getOne(param.id)
+    }
+}
