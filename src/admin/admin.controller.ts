f2c446deec40967640e955e2f5e6a0fbb8cefe26
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-09-06 20:12:01 +0300

    Jwt and role protection for post, delete, put methods in "AdminController"

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,11 +1,17 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { AdminDto } from './adminDto';
+import { Role } from '../auth/role.enum';
+import { Roles } from '../auth/roles.decorator';
+import { JwtStrategy } from 'src/auth/jwt.strategy';
+
 
 @Controller('admin')
 export class AdminController {
     constructor(private adminSevrice: AdminService) {}
 
+
+    
     @Get()
     getAll() {
         return this.adminSevrice.getAll();
@@ -16,17 +22,23 @@ export class AdminController {
         return this.adminSevrice.getOne(params.id);
     }
 
+    @UseGuards(JwtStrategy)
     @Post('create')
+    @Roles(Role.OWNER)
     create(@Body() adminDto: AdminDto) {
         return this.adminSevrice.create(adminDto);
     }
 
+    @UseGuards(JwtStrategy)
     @Delete(':id')
+    @Roles(Role.OWNER)
     delete(@Param() params) {
         return this.adminSevrice.delete(params.id);
     }
 
+    @UseGuards(JwtStrategy)
     @Put(':id')
+    @Roles(Role.OWNER)
     async update(@Param() param, @Body() adminDto: AdminDto) {
         const response = await this.adminSevrice.update(param.id, adminDto);
         return response;
