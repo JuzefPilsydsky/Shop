5b63f68fee5c1575dde8e130c93c24c7517d45c6
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-09-26 13:52:28 +0300

    Fixed circulare dependencies

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,14 +1,17 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminORM } from './adminORM';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([AdminORM])
+    TypeOrmModule.forFeature([AdminORM]),
+    forwardRef(() => AuthModule),
   ],
   providers: [AdminService],
-  controllers: [AdminController]
+  controllers: [AdminController],
+  exports: [AdminService],
 })
 export class AdminModule {}
