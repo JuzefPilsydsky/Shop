5a0d7c29b648dce0e95730f4ec640353e8d82106
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-09-26 14:02:05 +0300

    Added new import

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -5,12 +5,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductORM } from './productORM';
 import { CategoryService } from 'src/category/category.service';
 import { CategoryORM } from 'src/category/categoryORM';
+import { AuthModule } from 'src/auth/auth.module';
+import { JwtStrategy } from 'src/auth/jwt.strategy';
+import { AdminService } from 'src/admin/admin.service';
+import { AdminModule } from 'src/admin/admin.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([ProductORM, CategoryORM])
+    TypeOrmModule.forFeature([ProductORM, CategoryORM]),
+    AuthModule,
   ],
-  providers: [ProductService, CategoryService],
+  providers: [ProductService, CategoryService,],
   controllers: [ProductController]
 })
 export class ProductModule {}
