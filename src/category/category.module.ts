6fa8138bb7a90bc4b8f06a2f4dab73726f998ece
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-09-26 13:59:39 +0300

    Added nes import

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
 import { CategoryORM } from './categoryORM';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([CategoryORM])],
+  imports:[
+    TypeOrmModule.forFeature([CategoryORM]),
+    AuthModule
+  ],
   controllers: [CategoryController],
   providers: [CategoryService]
 })
